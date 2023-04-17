818380b053ed9a3d93e15a3ce7600139d47019d6
prjktalix <121919533+prjktalix@users.noreply.github.com>
2023-04-17 16:27:00 -0700

    FINISH CODE

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,9 +1,9 @@
 class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x ,y, texture, frame); {
-            scene.add.existing(this);   // add to existing scene
-            this.points = pointValue;   // store pointValue
-            this.moveSpeed = 3;          // pixel per frame
+            scene.add.existing(this);                           // add to existing scene
+            this.points = pointValue;                           // store pointValue
+            this.moveSpeed = game.settings.spaceshipSpeed;      // pixel per frame
         }
     }
 
@@ -12,7 +12,12 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         this.x -= this.moveSpeed;
         // wrap around from left edge to right edge
         if(this.x <= 0 - this.width){
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+
+    // position reset
+    reset(){
+        this.x = game.config.width;
     }
 }
